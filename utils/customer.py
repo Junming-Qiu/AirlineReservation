65f196661ecd080a91b0e3830721e337d875e8b3
Junming Qiu <junming@10-18-27-97.dynapool.wireless.nyu.edu>
2022-11-29 15:47:15 -0500

    commit

---
+++ utils/customer.py
@@ -0,0 +1,327 @@
+from utils.general import *
+import random
+import string
+
+
+
+# USE CASE 1: view purchased flights
+
+# return a joined table containing flights purchased by email (DO NOT CALL)
+def _my_flight_table(EMAIL: str):
+    sql = f'''
+    FROM flight as f, ticket as t, purchase as p,
+        airport as ap_origin, airport as ap_dest
+    WHERE f.flight_num=t.flight_num
+        AND f.airline=t.airline
+        AND f.dept_datetime=t.dept_datetime
+        AND f.origin=ap_origin.name
+        AND f.destination=ap_dest.name
+        AND t.id=p.ticket_id
+        AND p.customer_email='{EMAIL}' '''
+    return sql
+
+# view the flights purchased by a customer (optionally, given some filter param)
+def customer_view_my_flights(EMAIL: str, mysql,
+                             START_DATE=None, END_DATE=None,
+                             AP_ORIGIN=None, AP_DEST=None,
+                             CITY_ORIGIN=None, CITY_DEST=None) ->  list[tuple]:
+    sql = f'''
+    SELECT f.flight_num, f.airline, f.dept_datetime, f.status {_my_flight_table(EMAIL)} '''
+
+    if START_DATE: # NOT NULL
+        sql+=f'''
+        AND f.dept_datetime>={START_DATE})'''
+
+    if END_DATE:
+        sql+=f'''
+        AND f.dept_datetime<={END_DATE} '''
+
+    if AP_ORIGIN:
+        sql+=f'''
+        AND f.origin='{AP_ORIGIN}' '''
+
+    if AP_DEST:
+        sql+=f'''
+        AND f.destination='{AP_DEST}' '''
+
+    if CITY_ORIGIN:
+        sql+=f'''
+        AND ap_origin.name='{CITY_ORIGIN}' '''
+
+    if CITY_DEST:
+        sql+=f'''
+        AND ap_dest.name='{CITY_DEST}' '''
+
+    sql+=';'
+    return exec_sql(sql, mysql)
+
+
+
+
+# USE CASE 2: view future flights
+
+# search for two-way flights
+def customer_view_twoway_flights(START_DATE=None, END_DATE=None,
+                                        AP_ORIGIN=None, AP_DEST=None,
+                                        CITY_ORIGIN=None, CITY_DEST=None) ->  list[tuple]:
+    sql = '''
+        SELECT DISTINCT f1.origin, f1.destination,
+        f1.flight_num, f1.airline, f1.dept_datetime,
+        f2.flight_num, f2.airline, f2.dept_datetime,
+        f1.base_price+f2.base_price as combined_base_price
+        FROM flight as f1, flight as f2,
+        airport as ap_origin, airport as ap_dest
+        WHERE f1.origin=ap_origin.name
+            AND f1.destination=ap_dest.name
+            AND f1.destination=f2.origin
+            AND f1.origin=f2.destination
+            AND f1.arrv_datetime>f2.dept_datetime'''
+    if START_DATE:  # NOT NULL
+        sql += f'''
+        AND f1.dept_datetime>={START_DATE}'''
+    else:
+        today = date_in_X_days(0)
+        sql+=f'''
+        AND f1.dept_datetime>={today}'''
+
+    if END_DATE:
+        sql += f'''
+        AND f2.dept_datetime<={END_DATE} '''
+
+    if AP_ORIGIN:
+        sql += f'''
+        AND f1.origin='{AP_ORIGIN}' '''
+
+    if AP_DEST:
+        sql += f'''
+        AND f1.destination='{AP_DEST}' '''
+
+    if CITY_ORIGIN:
+        sql += f'''
+        AND ap_origin.name='{CITY_ORIGIN}' '''
+
+    if CITY_DEST:
+        sql += f'''
+        AND ap_dest.name='{CITY_DEST}' '''
+
+    sql+=';'
+    return sql
+
+
+# search for one-way flights
+def customer_view_oneway_flights(START_DATE=None, END_DATE=None,
+                                        AP_ORIGIN=None, AP_DEST=None,
+                                        CITY_ORIGIN=None, CITY_DEST=None) ->  list[tuple]:
+    sql = '''
+    SELECT DISTINCT f.origin, f.destination, 
+    f.flight_num, f.airline, f.dept_datetime, f.base_price
+    FROM flight as f, airport as ap_origin, airport as ap_dest
+    WHERE f.origin=ap_origin.name
+        AND f.destination=ap_dest.name'''
+
+    if START_DATE: # NOT NULL
+        sql+=f'''
+        AND f.dept_datetime>={START_DATE}'''
+    else:
+        today = date_in_X_days(0)
+        sql+=f'''
+        AND f.dept_datetime>={today}'''
+
+
+    if END_DATE:
+        sql+=f'''
+        AND f.dept_datetime<={END_DATE} '''
+
+    if AP_ORIGIN:
+        sql+=f'''
+        AND f.origin='{AP_ORIGIN}' '''
+
+    if AP_DEST:
+        sql+=f'''
+        AND f.destination='{AP_DEST}' '''
+
+    if CITY_ORIGIN:
+        sql+=f'''
+        AND ap_origin.name='{CITY_ORIGIN}' '''
+
+    if CITY_DEST:
+        sql+=f'''
+        AND ap_dest.name='{CITY_DEST}' '''
+
+    sql+=';'
+    return sql
+
+
+
+# USE CASE 3: purchase tickets
+
+# make unique, unused ticket id (DO NOT CALL)
+def _generate_ticket_id(mysql) -> str:
+
+    id=''
+    sql = f'''
+    SELECT *
+    FROM purchase
+    WHERE purchase.ticket_id='{id}';'''
+
+    while True:
+        id = ''.join(random.choices(string.hexdigits, k=7)).upper()
+        if not (exec_sql(sql,mysql)):
+            return id
+
+# make a new row in ticket (DO NOT CALL)
+def _create_ticket(FNUM: str, AIRLINE: str, DEPT_DT: str, mysql) -> str:
+    tid=_generate_ticket_id(mysql)
+    sql=f'''
+    INSERT INTO ticket
+    VALUES ('{tid}','{FNUM}','{AIRLINE}',{DEPT_DT});'''
+    exec_sql(sql,mysql,commit=True)
+    return tid
+
+# check if a card number exists (DO NOT CALL)
+def _card_exists(NUM: str, mysql) -> bool:
+    sql=f'''
+    SELECT *
+    FROM card_info
+    WHERE card_number='{NUM}';
+    '''
+    return exec_sql(sql, mysql)
+
+# make a new row in card info (DO NOT CALL)
+def _create_card_info(NUM: str, EXPR: str, NAME: str,
+                      TYPE: str, mysql) -> None:
+
+    if TYPE not in ('credit', 'debit'):
+        raise Exception('Invalid card type')
+
+    if not (_card_exists(NUM)):
+        sql=f'''
+        INSERT INTO card_info
+        VALUES ('{NUM}',{EXPR},'{NAME}','{TYPE}');
+        '''
+        exec_sql(sql,mysql,commit=True)
+
+# make a new row in purchase (DO NOT CALL)
+def _create_purchase(EMAIL: str, TID: str, PURCHASE_DT: str, SOLD_PRICE: str,
+                     BASE_PRICE: str, CARD_NUM: str, mysql) -> None:
+
+    sql=f'''
+    INSERT INTO purchase
+    VALUES ('{EMAIL}','{TID}',{PURCHASE_DT},{SOLD_PRICE},{BASE_PRICE},'{CARD_NUM}');
+    '''
+    exec_sql(sql, mysql, commit=True)
+
+# purchase a ticket of a flight TODO CALL THIS FUNCTION
+def customer_purchase_ticket(FNUM: str, AIRLINE: str, DEPT_DT: str,         # flight info
+                             SPRICE: str, BPRICE: str,
+                             CNUM: str, EXPR: str, NAME: str, TYPE: str,    # card info
+                             EMAIL: str,                                    # customer info
+                             mysql):
+
+    # ?. check for available seats
+
+    # 1. make ticket
+    ticket_id=_create_ticket(FNUM, AIRLINE, DEPT_DT, mysql)
+
+    # 2. check for card info / make card info
+    try:
+        _create_card_info(CNUM, EXPR, NAME, TYPE)
+    except:
+        pass
+
+    # 3. make purchase
+    today = date_in_X_days(0)
+    _create_purchase(EMAIL, ticket_id, today, SPRICE, BPRICE, CNUM)
+
+
+
+# USE CASE 4: cancel a trip
+
+# check that a ticket exists in purchase table with customer email (DO NOT CALL)
+def _validate_ticket_ownership(EMAIL, TID, mysql):
+    sql=f'''
+    SELECT *
+    FROM purchase
+    WHERE customer_email='{EMAIL}'
+        AND ticket_id='{TID}';
+    '''
+    return exec_sql(sql, mysql)
+
+# check that a ticket exists in ticket table (DO NOT CALL)
+def _validate_ticket_exists(TID, mysql):
+    sql=f'''
+    SELECT * 
+    FROM ticket
+    WHERE id='{TID}';
+    '''
+    return exec_sql(sql, mysql)
+
+# remove a row from ticket
+def customer_cancel_ticket(EMAIL, TID, mysql):
+    if _validate_ticket_ownership(EMAIL, TID, mysql): # ticket was purchased by customer
+        if _validate_ticket_exists(TID, mysql): # ticket was not already cancelled
+            sql=f'''
+            DELETE FROM ticket
+            WHERE id='{TID}';
+            '''
+            exec_sql(sql, mysql, commit=True)
+        else:
+            raise Exception('Ticket already was cancelled')
+    else:
+        raise Exception('Ticket not associated with customer email')
+
+# USE CASE 5: create flight_review
+
+# check if ticket id exists in ticket & purchase,
+#   in purchase with customer email (DO NOT CALL)
+def _validate_customer_took_flight(EMAIL, TID, mysql):
+    sql = f'''
+    SELECT *
+    FROM purchase as p, ticket as t
+    WHERE p.ticket_id=t.id
+        AND p.customer_email='{EMAIL}'
+        AND t.id='{TID}';
+    '''
+    exec_sql(sql, mysql)
+
+# make a new row in flight review
+def customer_create_flight_review(EMAIL, TID, RATING, COMMENT, mysql):
+
+    if _validate_customer_took_flight(EMAIL, TID, mysql): # customer was on flight
+        sql=f'''
+        INSERT INTO flight_rating
+        VALUES ('{EMAIL}', '{TID}', {RATING}, '{COMMENT}');
+        '''
+        exec_sql(sql, mysql, commit=True)
+    else:
+        raise Exception('No available record of flight attendance')
+
+
+
+
+# USE CASE 6: track spending
+
+def customer_view_spending_interval(EMAIL, START, END, mysql) -> int:
+    sql=f'''
+    SELECT sum(sold_price)
+    FROM purchase
+    WHERE purchase_datetime BETWEEN {START} AND {END}
+    GROUP BY customer_email
+    HAVING customer_email={EMAIL};
+    '''
+    rtn=exec_sql(sql, mysql)
+    return rtn[0]
+
+def customer_view_spending_past6months(EMAIL, mysql) -> int:
+    start = date_in_X_days(-183)
+    end = date_in_X_days(0)
+    return customer_view_spending_interval(EMAIL, start, end, mysql)
+
+def customer_view_spending_pastyear(EMAIL, mysql) -> int:
+    start = date_in_X_days(-365)
+    end = date_in_X_days(0)
+    return customer_view_spending_interval(EMAIL, start, end, mysql)
+
+
+
+# USE CASE 7: logout
---
+++ utils/general.py
@@ -0,0 +1,213 @@
+import hashlib
+import datetime
+from flask_mysqldb import MySQL
+
+
+#############################################################
+
+# *******
+# GENERAL
+# *******
+
+
+# TODO: Wrap SQL statement with Atomic Transaction
+def sql_transaction_wrap(sql: str) -> str:
+    wrap_tmp = f'''
+    USE flight_app;
+    
+    {sql}
+    '''
+    #
+    # START TRANSACTION
+    #
+    # {sql}
+    #
+    # if ....
+    #     COMMIT;
+    #
+    # then ...
+    #     ROLLBACK;
+    # '''
+
+    return wrap_tmp
+
+
+# execute a sql statement
+def exec_sql(sql: str, mysql, commit=False) -> list:
+    cur = mysql.connection.cursor()
+    cur.execute(sql)
+    if commit:
+        mysql.connection.commit()
+    return cur.fetchall()
+
+
+# returns an encrypted password
+def encrypt_password(password: str) -> str:
+    hash_object = hashlib.md5(password.encode())
+    return hash_object.hexdigest()
+
+
+def check_datetime_format(DATE: str) -> None:
+    try:
+        datetime.datetime.strptime(DATE, '%Y%m%d %H%M%S')
+    except ValueError:
+        raise ValueError(f'Incorrect date format {DATE}, must be YYYYMMDD')
+
+
+def date_in_X_days(NUM_DAYS: int) -> str:
+    today_plus_X = str(datetime.date.today() + datetime.timedelta(days=NUM_DAYS))
+    time_str = today_plus_X[0:4] + today_plus_X[5:7] + today_plus_X[8:10]
+    check_datetime_format(time_str)
+    return time_str
+
+
+# ******
+# LOG IN
+# ******
+
+def store_verify(session, customer_tokens, staff_tokens):
+    username_or_email = ""
+    key = ""
+    c_logged = False
+    s_logged = False
+
+    if "username" in session:
+        username_or_email = session["username"]
+    if "key" in session:
+        key = session["key"]
+
+    if username_or_email in customer_tokens:
+        if customer_tokens[username_or_email] == key:
+            c_logged = True
+
+    if username_or_email in staff_tokens:
+        if staff_tokens[username_or_email] == key:
+            s_logged = True
+
+    return c_logged, s_logged
+
+
+# check if a staff's log in credentials exists
+def query_staff_credentials(USERNAME: str, PASSWORD: str, mysql) -> list:
+    ENCRYPTED_PASSWORD = encrypt_password(PASSWORD)
+    #print(f'MySQL: Query Staff {USERNAME} {ENCRYPTED_PASSWORD}')
+    sql = f'''
+        SELECT *
+        FROM airline_staff
+        WHERE username = '{USERNAME}'
+            AND password = '{ENCRYPTED_PASSWORD}';
+        '''
+    return exec_sql(sql, mysql)
+
+
+# check if a customer's log in credentials exists
+def query_customer_credentials(EMAIL: str, PASSWORD: str, mysql) -> list:
+    ENCRYPTED_PASSWORD = encrypt_password(PASSWORD)
+    sql = f'''
+        SELECT *
+        FROM customer
+        WHERE email = '{EMAIL}'
+            AND password = '{ENCRYPTED_PASSWORD}';
+        '''
+    return exec_sql(sql, mysql)
+
+
+# ******************
+# STAFF REGISTRATION
+# ******************
+
+# check if a staff's username exists
+def query_staff_username(USERNAME: str, mysql) -> bool:
+    sql = f'''
+    SELECT *
+    FROM airline_staff
+    WHERE username = '{USERNAME}';
+    '''
+    username_exists = exec_sql(sql, mysql)
+    if username_exists:
+        return True
+    return False
+
+
+# check if a staff's employer exists
+def query_staff_employer(EMPLOYER: str, mysql) -> bool:
+    sql = f'''
+    SELECT *
+    FROM airline
+    WHERE name = '{EMPLOYER}';
+    '''
+    employer_exists = exec_sql(sql, mysql)
+    if employer_exists:
+        return True
+    return False
+
+
+# create a staff account
+def create_staff_account(USERNAME: str, PASSWORD: str, FNAME: str,
+                         LNAME: str, DOB: str, EMPLOYER: str, mysql) -> None:
+    ENCRYPTED_PASSWORD = encrypt_password(PASSWORD)
+    sql = f'''
+    INSERT INTO airline_staff
+    VALUES ('{USERNAME}','{ENCRYPTED_PASSWORD}','{FNAME}',
+        '{LNAME}','{DOB}','{EMPLOYER}');
+    '''
+    exec_sql(sql, mysql, commit=True)
+
+
+# *********************
+# CUSTOMER REGISTRATION
+# *********************
+
+# check if a customer's email exists
+def query_customer_email(EMAIL: str, mysql) -> bool:
+    sql = f'''
+    SELECT *
+    FROM customer
+    WHERE email = '{EMAIL}';
+    '''
+
+    account_exists = exec_sql(sql, mysql)
+    if account_exists:
+        return True
+    return False
+
+
+# create a customer account
+def create_customer_account(EMAIL: str, NAME: str, PASSWORD: str, BUILDING_NUM: str, CITY: str, STATE: str,
+                            STREET: str, PP_COUNTRY: str, PP_NUM: str, PP_EXPR: str, DOB: str,
+                            PHONE_NUM: str, mysql) -> None:
+    ENCRYPTED_PASSWORD = encrypt_password(PASSWORD)
+    sql = f'''
+    INSERT INTO customer
+    VALUES ('{EMAIL}','{NAME}','{ENCRYPTED_PASSWORD}','{BUILDING_NUM}','{CITY}','{STATE}',
+            '{STREET}','{PP_COUNTRY}','{PP_NUM}','{PP_EXPR}','{DOB}','{PHONE_NUM}');
+    '''
+    exec_sql(sql, mysql, commit=True)
+
+
+# Takes many works and checks that they are valid as a group
+def parse_input(inputs: list[str], ispass=False) -> bool:
+    alpha_lower = "abcdefghijklmnopqrstuvwxyz"
+    alpha_upper = alpha_lower.capitalize()
+    nums = "0123456789"
+    restrict = ['\0', '\'', '\"', '\b', '\n', '\r', '\t', '\Z', '\\', '\%', '\_', \
+        '?', '-', '(', ')', '{', '}', '[', ']']
+
+    for word in inputs:
+        if word == None:
+            continue
+
+        if ispass:
+            if ' ' in word:
+                print(f"failed to parse {word}")
+                return False
+
+            if len(word) <= 8:
+                print(f"failed to parse {word}")
+                return False
+
+        for c in word:
+            if c not in alpha_lower and c not in alpha_upper and c not in nums and c in restrict:
+                print(f"failed to parse {word}")
+                return False
+    return True
